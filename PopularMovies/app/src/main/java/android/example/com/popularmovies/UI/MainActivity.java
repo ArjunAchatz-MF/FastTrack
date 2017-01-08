b55027030c3e8c5ae4de851f58514a4ebe40a2d8
Arjun Achatz <arjun@matterandform.net>
2017-01-07 19:49:54 -0500

    Starting new project in master

---
+++ PopularMovies/app/src/main/java/android/example/com/popularmovies/UI/MainActivity.java
@@ -0,0 +1,14 @@
+package android.example.com.popularmovies.UI;
+
+import android.example.com.popularmovies.R;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
